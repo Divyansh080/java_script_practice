5657ce740e661ace50b32cb0475e775753e5b1c2
Divyansh Makwana <154442244+Divyansh080@users.noreply.github.com>
2024-12-13 12:56:56 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,17 @@
+console.log("hello");
+const AccountId =102;
+let EmpName= "divyansh";
+let Email = "dm@dm.com";
+var Salary = "60000";
+EmpRoomNo = "456";
+let account; // undefined
+
+// let AccountId =120;   do not change the value of constant variable.
+
+console.log(AccountId); // use to disply single value.
+console.log([AccountId, EmpName, Email, Salary]); // use to display value in tabluler form.
+console.log(account);
+console.log(EmpRoomNo);
+
+/*var boss ="i am here";
+            Do not use "var" to declare variable cause of function scope and block scope. */
